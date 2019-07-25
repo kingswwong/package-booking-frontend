11bc27dd8d7eab08ce9c857b6a74b3e1dd9ffd73
“Dean” <467271883@qq.com>
2019-07-25 10:57:11 +0800

    Add NavBar

---
+++ src/components/Menu.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="header">
+    <el-menu
+      :default-active="activeIndex"
+      class="el-menu-demo"
+      mode="horizontal"
+      @select="handleSelect"
+      background-color="#545c64"
+      text-color="#fff"
+      active-text-color="#ffd04b">
+      <el-menu-item index="1"><a href="#">订单管理</a></el-menu-item>
+    </el-menu>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Menu',
+  data () {
+    return {
+      activeIndex: '1'
+    }
+  },
+  methods: {
+    handleSelect (key, keyPath) {
+      console.log(key, keyPath)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-container>
+      <el-header>
+        <Menu/>
+      </el-header>
+      <el-main>Main</el-main>
+    </el-container>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
+import Menu from '../components/Menu'
 export default {
   name: 'home',
-  components: {
-    HelloWorld
-  }
+  components: { Menu }
 }
 </script>
