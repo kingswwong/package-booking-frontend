b853fb7f769d98150c484e6e101994bc0407d4ad
“Dean” <467271883@qq.com>
2019-07-25 11:00:43 +0800

    Delete ElmentUI and import antdesign

--- src/main.js
+++ src/main.js
@@ -2,10 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
+import 'ant-design-vue/dist/antd.css'
+import Antd from 'ant-design-vue'
 
-Vue.use(ElementUI)
+Vue.use(Antd)
 Vue.config.productionTip = false
 
 new Vue({
